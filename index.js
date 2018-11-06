da9e9219ddea7d3f68645c2cd970a4efd99b58d5
Patrick A Kim <pkim050@gmail.com>
2018-11-06 23:09:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, object[key] = value);
 }
 
+function destructivelyUpdateObjetWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
+
